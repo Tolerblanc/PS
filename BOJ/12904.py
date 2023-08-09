69b7b3906a826d41fcc08594e06a29b6a136d498
HyunJun KIM <hihj070914@icloud.com>
2023-08-09 21:31:58 +0900

    add 12904.py

---
+++ BOJ/12904.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+s = list(input().rstrip())
+t = list(input().rstrip())
+while (len(s) != len(t)):
+	if (t[-1] == 'A'):
+		t.pop()
+	elif (t[-1] == 'B'):
+		t.pop()
+		t.reverse()
+print(1 if s == t else 0)
