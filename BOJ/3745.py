398cafde32ae25a2306f15a8957e6e0b9be8edc8
HyunJun <hyunjuki@student.42seoul.kr>
2023-08-21 13:02:24 +0900

    add 3745.py

--- BOJ/3745.py
+++ BOJ/3745.py
@@ -5,7 +5,7 @@ input = sys.stdin.readline
 
 def find_lis(n, seq):
 	dp = [0]
-	for i, s in enumerate(seq):
+	for s in seq:
 		idx = bisect_left(dp, s)
 		if idx == len(dp) and dp[-1] < s:
 			dp.append(s)
