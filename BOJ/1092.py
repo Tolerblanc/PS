8cc901e04493111b49118a3fb73bcb900d7e42e8
HyunJun KIM <hihj070914@icloud.com>
2023-08-26 23:58:36 +0900

    add 1092.py

---
+++ BOJ/1092.py
@@ -0,0 +1,33 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n = int(input())
+crane = sorted(map(int, input().split()), reverse=True)
+m = int(input())
+box = deque(sorted(map(int, input().split()), reverse=True))
+
+
+def solve():
+    if box[0] > crane[0]:
+        return -1
+    count = 0
+    while box:
+        crane_cnt = 0
+        count += 1
+        for i in range(m):
+            b = box.popleft()
+            if crane_cnt < n:
+                if crane[crane_cnt] >= w:
+                    m -= 1
+                    crane_cnt += 1
+                else:
+                    box.append(box)
+            else:
+                box.append(box)
+        if (count >= 10000):
+            break
+    return count
+
+
+print(solve())
