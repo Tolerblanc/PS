51bdf875d454d6252b00a2e9613b22d34c97235a
Hyunjun KIM <hihj070914@icloud.com>
2024-03-01 14:52:48 +0900

    add 15651.py

---
+++ BOJ/15651.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+result = []
+
+
+def backtracking(depth):
+    if depth == m:
+        print(*result)
+        return
+    for i in range(1, n+1):
+        result.append(i)
+        backtracking(depth+1)
+        result.pop()
+
+
+backtracking(0)
