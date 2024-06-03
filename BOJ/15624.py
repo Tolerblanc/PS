1ff7c6c01a2cc994ea7039418801e5dfee79b448
Hyunjun KIM <hihj070914@icloud.com>
2024-06-03 13:49:20 +0900

    add 15624.py

---
+++ BOJ/15624.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+
+def fibo(n):
+    if n <= 3:
+        return n
+    prev, curr = 1, 2
+    MOD = 1_000_000_007
+    for i in range(2, n):
+        prev, curr = curr % MOD, (prev + curr) % MOD
+    return curr % MOD
+
+
+print(fibo(int(input())))
