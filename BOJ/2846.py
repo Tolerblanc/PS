8bcb8f9a9bc9b492d2862ca1c6928f9210586ea6
Hyunjun KIM <hihj070914@icloud.com>
2024-06-26 11:10:37 +0900

    fix 2846.py

--- BOJ/2846.py
+++ BOJ/2846.py
@@ -5,10 +5,11 @@ n = int(input())
 lst = list(map(int, input().split()))
 diff = [lst[i] - lst[i - 1] for i in range(1, n)]
 answer = tmp = 0
+print(diff)
 for d in diff:
     if d <= 0:
         answer = max(answer, tmp)
         tmp = 0
     else:
         tmp += d
-print(answer)
+print(max(answer, tmp))
