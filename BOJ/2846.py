0120beeaff4a9fde1ed19ddd5d5bb2090f14a0ee
Hyunjun KIM <hihj070914@icloud.com>
2024-06-26 11:07:15 +0900

    add 2846.py

---
+++ BOJ/2846.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+lst = list(map(int, input().split()))
+diff = [lst[i] - lst[i - 1] for i in range(1, n)]
+answer = tmp = 0
+for d in diff:
+    if d <= 0:
+        answer = max(answer, tmp)
+        tmp = 0
+    else:
+        tmp += d
+print(answer)
