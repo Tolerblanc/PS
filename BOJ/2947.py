4412edcd02431f390b210c36a39412b8dd61db15
Hyunjun KIM <hihj070914@icloud.com>
2024-06-28 00:30:15 +0900

    add 2947.py

---
+++ BOJ/2947.py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+
+nums = list(map(int, input().split()))
+while nums != [1, 2, 3, 4, 5]:
+    for i in range(4):
+        if nums[i] > nums[i + 1]:
+            nums[i], nums[i + 1] = nums[i + 1], nums[i]
+            print(*nums)
